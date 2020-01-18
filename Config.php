303dab8d1375da579a42a3677957323061674816
amlanuet <amlanuet@users.noreply.github.com>
2020-01-18 14:24:06 +0100

    Initial commit

---
+++ Config.php
@@ -0,0 +1,16 @@
+<?php
+$currency = '€ '; //Currency Character or code
+
+$db_username = 'root';
+$db_password = '';
+$db_name = 'pizza';
+$db_host = 'localhost';
+
+$shipping_cost      = 1.50; //shipping cost
+						
+//connect to MySql						
+$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);						
+if ($mysqli->connect_error) {
+    die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+}
+?>
